5347ef8928f0fb00ebdff9b9fd80fa9007918fae
set-page <bobvchuck@gmail.com>
2025-11-12 14:00:36 +0800

    https://api.github.com/repos/set-page/set-page.github.io/contents/sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-self.addEventListener('push', event => {
-  console.log('[ServiceWorker] Push diterima');
-  if (!event.data) {
-    console.warn('[ServiceWorker] event.data kosong!');
-    return;
-  }
-  try {
-    const data = event.data.json();
-    console.log('[ServiceWorker] Payload:', data);
-    event.waitUntil(
-      self.registration.showNotification(data.data.title, {
-        body: data.data.body,
-        data: { url: data.data.url }
-      })
-    );
-  } catch (e) {
-    console.error('[ServiceWorker] Gagal parse:', e);
-  }
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10176922
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
