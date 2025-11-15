bb78df4fff9f02e30070294b2b00ba5750105a62
set-page <bobvchuck@gmail.com>
2025-11-15 14:23:56 +0800

    https://api.github.com/repos/set-page/set-page.github.io/contents/sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10189478
+    "domain": "3nbf4.com",
+    "zoneId": 10176954
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
