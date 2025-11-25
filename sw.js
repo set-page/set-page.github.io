1c98641eec382168b724f315be786ee2d40ee59d
set-page <bobvchuck@gmail.com>
2025-11-25 17:51:23 +0800

    https://api.github.com/repos/set-page/set-page.github.io/contents/sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-self.addEventListener('push', event => {
-  console.log('[ServiceWorker] Push diterima');
-  if (!event.data) {
-    console.warn('[ServiceWorker] event.data kosong!');
-    return;
-  }
-  try {
-    const data = event.data.json();
-    console.log('[ServiceWorker] Payload:', data);
-    event.waitUntil(
-      self.registration.showNotification(data.data.title, {
-        body: data.data.body,
-        data: { url: data.data.url }
-      })
-    );
-  } catch (e) {
-    console.error('[ServiceWorker] Gagal parse:', e);
-  }
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+
+  const title = data.title || "Notifikasi";
+  const options = {
+    body: data.body || "Ada pesan baru.",
+    icon: data.icon || "/icon.png",
+    badge: data.badge || "/badge.png"
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(data.url || "/")
+  );
 });
